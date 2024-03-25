a99c0319903704260be36383cf3feb46e68a258e
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-03-25 23:50:51 +0300

    25 mart 2.mola

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -7,12 +7,9 @@ using System.Threading.Tasks;
 
 namespace Business.Abstract
 {
-    public interface IProductService
+    public interface ICategoryService
     {
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
-
-
+        List<Category> GetAll();
+        Category GetById(int categoryId);
     }
 }
--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Concrete.EntityFramework
+namespace DataAccess.Concrete.EntityFramework 
 {
     //Context   :   Db tabloları ile proje classlarını bağlamak
     public class NorthwindContext:DbContext
@@ -18,5 +18,6 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }  
+        public DbSet<Order> Orders { get; set; }
     }
 }
