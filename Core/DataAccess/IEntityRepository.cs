988d40cd0deb1866fa3139ed77e81ad486e65eec
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-03-25 22:02:46 +0300

    Core Layer added myProject

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,5 +1,5 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +7,12 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint (kısıtlama)
     //class : referans tipli olabilir.
     //IEntity :IEntity olabilir veya IEntity implemente eden bir nesne olabilir
-    //new() : new'lenebilir olmalı.
+    //new() : new'lenebilir olmalı. 
     public interface IEntityRepository<T>where T:class,IEntity,new()
     {
         
