b4c27b9eef7630e2e1f219084dbbc95b051a92bf
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-03-28 11:21:24 +0300

    Project büyüyor yavaş yavaş

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -41,29 +41,29 @@ namespace Business.Concrete
             //İş kodları.
             if (DateTime.Now.Hour == 22)
             {
-                return new ErrorResult();
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
-            return new DataResult<List<Product>>(_productDal.GetAll(),true,"Ürünler Listelendi");
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed); 
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p=>p.CategoryId == id);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId == id));
         }
 
-        public Product GetById(int productId)
-        {
-            return _productDal.Get(p=>p.ProductId == productId);
+        public IDataResult<Product> GetById(int productId)
+        {  //Yapı basit aslında SuccesDataResult<T> formantının içine Product gönderiyoruz parametre olarak da parantez içindekiler.
+            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId == productId));
         }
 
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p=>p.UnitPrice >=min && p.UnitPrice <=max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.UnitPrice >=min && p.UnitPrice <=max));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        //Loosely coupled gevşek bağlılık.
+        //naming convertion
+        //IoC Container -->Inversion of Control
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result=_productService.GetAll();
+            return result.Data;
+
+            //Dependency chain-- Bagımlılık zinciri
+            //IProductService productService = new ProductManager(new EfProductDal());
+            //var result = productService.GetAll();
+            //return result.Data;
+           
+        }
+    }
+}
