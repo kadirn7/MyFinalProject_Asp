3791990ae675605ecda40bfc8cb13b0252a75881
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-04-25 02:50:59 +0300

    Attribute and Validation added

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using System;
@@ -20,6 +23,17 @@ namespace Business.DependencyResolvers.Autofac
             //singleInstance tek bir Instance tutar. Herkese aynı Instance'ı verir. //newleme işlemlerini bunlar yapar.
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
+
         }
     }
 }
---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,32 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using System.Linq;
+using IInterceptor = Castle.DynamicProxy.IInterceptor;
+
+namespace Core.Utilities.Interceptors
+{
+
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+            //   classAttributes.Add(new ExceptionLogAspect(typeof(Filelogger))); //Otomatik olarak sistemmdeki bütün methodları log'a  dahil etmeye yarıyor
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray(); //öncelik değerine göre sıralama yapılır.
+        }
+    }
+     
+     
+}
