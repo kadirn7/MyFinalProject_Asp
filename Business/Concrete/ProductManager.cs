5440e2090ceffb1e772cc8b0a506de25b06114cb
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-04-25 01:46:44 +0300

    Validation and Log

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,16 +1,21 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using DataAccess.DTOs;
 using Entities.Concrete;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Business.Concrete
 {
@@ -27,14 +32,9 @@ namespace Business.Concrete
         {
             //business codes
             //validation
-            if (product.UnitPrice <= 0)
-            {
-                return new ErrorResult(Messages.UnitPriceInvalid);
-            }
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            
+            ValidationTool.Validate(new ProductValidator(),product);
+             
             _productDal.Add(product); 
 
             return new SuccessResult(Messages.ProductAdded);
--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -17,7 +17,12 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(P => P.UnitPrice).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThan(0);
             RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
-               
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+           //kendi yazacagımız kural. //generate method(startWithA)
+        }
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
         }
     }
 }
