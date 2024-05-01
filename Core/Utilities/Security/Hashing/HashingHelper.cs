9750b9071445f2e0c1fc3f866e503b5ce352cbc7
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-05-01 15:04:50 +0300

    Token

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        //Yazılım geliştirme Kampı 14.gün
+        public static void CreatePasswordHash //ilk defa şifre belirleyen için Hashleme
+            (string password,out byte[] passwordHash,out byte[] passwordSalt)
+        { //out dışarıya verilecek değer //out birden fazla veriyi döndürmek için kullanılır.
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+        public static bool VerifyPasswordHash(string password,  byte[] passwordHash,  byte[] passwordSalt) //Doğrulama
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i=0;i<computedHash.Length;i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
+ 
